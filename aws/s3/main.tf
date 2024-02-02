658d4872a49990f54f495d5959e908982d3788fd
Viachaslau Vegero <viachaslau.vegero@testit.software>
2024-02-02 21:11:02 +0300

    fix bucket name

--- aws/s3/main.tf
+++ aws/s3/main.tf
@@ -1,7 +1,7 @@
 
 #tfsec:ignore:aws-s3-ignore-public-acls tfsec:ignore:aws-s3-enable-bucket-encryption tfsec:ignore:aws-s3-block-public-policy tfsec:ignore:aws-s3-block-public-acls tfsec:ignore:aws-s3-no-public-buckets tfsec:ignore:aws-s3-encryption-customer-key tfsec:ignore:aws-s3-enable-bucket-logging tfsec:ignore:aws-s3-enable-versioning tfsec:ignore:aws-s3-specify-public-access-block
 resource "aws_s3_bucket" "this" {
-  bucket = var.bucket_name
+  bucket = "${var.deployment_prefix}-${var.bucket_name}"
 }
 
 resource "aws_s3_bucket_acl" "this" {
