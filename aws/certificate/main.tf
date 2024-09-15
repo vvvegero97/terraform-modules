c2087eca1ad6fcc3868a0661bb9b910a70d8310b
Viachaslau Vegero <viachaslau.vegero@testit.software>
2024-09-15 15:35:36 +0300

    switch aws record from count to for-each

--- aws/certificate/main.tf
+++ aws/certificate/main.tf
@@ -11,14 +11,15 @@ data "aws_route53_zone" "selected_zones" {
 }
 
 resource "aws_route53_record" "validation" {
-  count           = length(aws_acm_certificate.this.domain_validation_options)
-  zone_id         = data.aws_route53_zone.selected_zones[aws_acm_certificate.this.domain_validation_options[count.index].domain_name].zone_id
-  name            = aws_acm_certificate.this.domain_validation_options[count.index].resource_record_name
-  type            = aws_acm_certificate.this.domain_validation_options[count.index].resource_record_type
-  ttl             = 60
-  allow_overwrite = true
-  records         = [aws_acm_certificate.this.domain_validation_options[count.index].resource_record_value]
+  for_each = { for idx, val in aws_acm_certificate.this.domain_validation_options : idx => val }
+
+  zone_id = data.aws_route53_zone.selected_zones[each.value.domain_name].zone_id
+  name    = each.value.resource_record_name
+  type    = each.value.resource_record_type
+  ttl     = 60
+  records = [each.value.resource_record_value]
 }
+
 
 resource "aws_acm_certificate_validation" "this" {
   certificate_arn         = aws_acm_certificate.this.arn
